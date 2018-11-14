57d6e1b4a6beed51c757a70ad4178f18998db133
Neel Patel <37004953+neel3108@users.noreply.github.com>
2018-11-14 16:05:35 -0500

    Add files via upload

---
+++ Lab9/ex4/customer.ts
@@ -0,0 +1,12 @@
+export class Customer {
+    private firstname : string;
+    private lastname : string;
+   
+    constructor(firstname: string, lastname: string){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    greeter = function(){
+        console.log(`Hello  ${this.firstname} ${this.lastname}`)
+    }
+}
